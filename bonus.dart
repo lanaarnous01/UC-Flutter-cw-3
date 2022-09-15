ec43be00ab2b9893552e3d5fca759b5dec734a76
lanaarnous01 <93349082+lanaarnous01@users.noreply.github.com>
2022-09-15 17:11:56 +0300

    bonus

--- bonus.dart
+++ bonus.dart
@@ -1,1 +1,14 @@
-void main() {}
+void main() {
+  var value = sum([1, 4, 2, 7]);
+  print(value);
+}
+
+double sum(List<int> total) {
+  var x = 0.0;
+
+  for (var i = 0; i < total.length; i++) {
+    x += total[i];
+  }
+  //print(x);
+  return x;
+}
